b5e15cc353d7bb85579c94de2ef56dcd734345a5
Pavan Kumar TULASI <pavan.kumar.tulasi@ing.com>
2019-03-21 11:59:01 +0100

    added the secret

--- app/src/main/java/com/example/appcenter/sampleapp_android/MainActivity.java
+++ app/src/main/java/com/example/appcenter/sampleapp_android/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        AppCenter.start(getApplication(), "<APP SECRET HERE>",
+        AppCenter.start(getApplication(), "4a2be60069fb2bd0695e7cb7a887838c498ad922",
                 Analytics.class, Crashes.class);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_root);
